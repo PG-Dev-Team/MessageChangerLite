69b8f4654f092e45df87df69f1325764f65b737e
Devil Boy <devilboy@pinoygamers.info>
2013-08-26 20:44:18 -0700

    Worked on ServerStop Message Configuration

--- src/pgDev/bukkit/MessageChangerLite/messages/ServerStopMessage.java
+++ src/pgDev/bukkit/MessageChangerLite/messages/ServerStopMessage.java
@@ -1,6 +1,11 @@
 package pgDev.bukkit.MessageChangerLite.messages;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.LinkedHashMap;
+import java.util.logging.Level;
 
 import org.bukkit.entity.Player;
 
@@ -40,6 +45,36 @@ public class ServerStopMessage extends Message {
 	
 	@Override
 	void generateFile() {
-		
+		BufferedWriter out = null;
+		try {
+			out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(messageDir)));
+			out.write("#\r\n");
+			out.write("# Server-Stop Message Configuration\r\n");
+			out.write("#\r\n");
+			out.write("# Format:\r\n");
+			out.write("#	<permission>=<message>\r\n");
+			out.write("#\r\n");
+			out.write("# Variables:\r\n");
+			out.write("#	%default% -> The default message of the server\r\n");
+			out.write("#	%receiverName% -> The username of the player receiving \r\n");
+			out.write("this message\r\n");
+			out.write("#	%receiverDisplayName% -> The display name of the \r\n");
+			out.write("player receiving this message\r\n");
+			out.write("\r\n");
+			out.write("default=Server is shutting down...\r\n");
+			out.write("\r\n");
+			out.write("admin=Your server is shutting down!\r\n");
+			out.write("vip=Sorry! Server going down for maintenance.\r\n");
+		} catch (Exception e) {
+			MessageChangerLite.logger.log(Level.SEVERE, "Could not generate Server-Stop Message Configuration", e);
+		} finally {
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {
+					MessageChangerLite.logger.log(Level.SEVERE, "Could not close Server-Stop Message Configuration write stream", e);
+				}
+			}
+		}
 	}
 }
